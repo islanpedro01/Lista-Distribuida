19d667ce50d5a7eefd2d26f9dfa44fda0ce803e9
Islan Pedro <113214808+islanpedro01@users.noreply.github.com>
2025-06-10 08:39:30 -0300

    Update remotelist_rpc.go

--- pkg/remotelist/remotelist_rpc.go
+++ pkg/remotelist/remotelist_rpc.go
@@ -27,28 +27,6 @@ type RemoteList struct {
 	// snapMutex protege apenas a parte de criar snapshot (para duplicar o map em memória)
 }
 
-// func (l *RemoteList) Append(value int, reply *bool) error {
-// 	l.mu.Lock()
-// 	defer l.mu.Unlock()
-// 	l.list = append(l.list, value)
-// 	fmt.Println(l.list)
-// 	l.size++
-// 	*reply = true
-// 	return nil
-// }
-
-// func (l *RemoteList) Remove(arg int, reply *int) error {
-// 	l.mu.Lock()
-// 	defer l.mu.Unlock()
-// 	if len(l.list) > 0 {
-// 		*reply = l.list[len(l.list)-1]
-// 		l.list = l.list[:len(l.list)-1]
-// 		fmt.Println(l.list)
-// 	} else {
-// 		return errors.New("empty list")
-// 	}
-// 	return nil
-// }
 
 func NewRemoteList() (*RemoteList, error) {
 	rl := &RemoteList{
